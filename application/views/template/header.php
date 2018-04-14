3a2b931194d531e97a5e03b8e904f1a96b89cc57
chiachintyaa <chintyarifyaningrum@yahoo.com>
2018-04-14 10:38:04 +0700

    added header and footer template

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,7 +1,81 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Chia
- * Date: 14/04/2018
- * Time: 9:49
- */
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Realestate Bootstrap Theme </title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.css')?>" />
+    <link rel="stylesheet" href="<?php echo base_url('assets/style.css')?>"/>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.js')?>"></script>
+    <script src="<?php echo base_url('assets/script.js')?>"></script>
+
+    <!-- Owl stylesheet -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/owl-carousel/owl.carousel.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/owl-carousel/owl.theme.css')?>">
+    <script src="<?php echo base_url('assets/owl-carousel/owl.carousel.js')?>"></script>
+    <!-- Owl stylesheet -->
+
+
+    <!-- slitslider -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/slitslider/css/style.css')?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/slitslider/css/custom.css')?>" />
+    <script type="text/javascript" src="<?php echo base_url('assets/slitslider/js/modernizr.custom.79639.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/slitslider/js/jquery.ba-cond.min.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/slitslider/js/jquery.slitslider.js')?>"></script>
+    <!-- slitslider -->
+</head>
+
+<body>
+<!-- Header Starts -->
+<div class="navbar-wrapper">
+
+    <div class="navbar-inverse" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+
+
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+            </div>
+            <!-- Nav Starts -->
+            <div class="navbar-collapse  collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="agents.php">Agents</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+            <!-- #Nav Ends -->
+
+        </div>
+    </div>
+
+</div>
+<!-- #Header Starts -->
+
+<div class="container">
+
+    <!-- Header Starts -->
+    <div class="header">
+        <a href="index.php"><img src="<?php echo base_url('assets/images/logo.png')?>" alt="Realestate"></a>
+
+        <ul class="pull-right">
+            <li><a href="buysalerent.php">Buy</a></li>
+            <li><a href="buysalerent.php">Sale</a></li>
+            <li><a href="buysalerent.php">Rent</a></li>
+        </ul>
+    </div>
+    <!-- #Header Starts -->
+</div>
