a1191ded3f1b6ca5050524fa6d2f9e6a49f3bc25
chiachintyaa <chintyarifyaningrum@yahoo.com>
2018-04-17 21:51:24 +0700

    added about us page, and modified css for menu links in agents and contact us page

--- application/views/about.php
+++ application/views/about.php
@@ -1,7 +1,115 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Chia
- * Date: 17/04/2018
- * Time: 20:10
- */
+defined('BASEPATH') OR exit('No direct script access allowed');
+#include header file
+$this->view('template/header');
+?>
+    <style>
+        .bg-img-1 {
+            background-image: url(<?php echo base_url('assets/images/slider/1.jpg')?>);
+        }
+        .bg-img-2 {
+            background-image: url(<?php echo base_url('assets/images/slider/2.jpg')?>);
+        }
+        .bg-img-3 {
+            background-image: url(<?php echo base_url('assets/images/slider/3.jpg')?>);
+        }
+        .bg-img-4 {
+            background-image: url(<?php echo base_url('assets/images/slider/4.jpg')?>);
+        }
+        .bg-img-5 {
+            background-image: url(<?php echo base_url('assets/images/slider/5.jpg')?>);
+        }
+    </style>
+    <!-- Header Starts -->
+    <div class="navbar-wrapper">
+
+        <div class="navbar-inverse" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+
+
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                </div>
+                <!-- Nav Starts -->
+                <style>
+                    .nav li a{
+                        font-family: 'Ubuntu', sans-serif !important;
+                    }
+                </style>
+                <div class="navbar-collapse  collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="<?php echo site_url()?>" >Home</a></li>
+                        <li><a href="<?php echo site_url('Agents')?>">Agents</a></li>
+                        <li><a href="<?php echo site_url('ContactUs')?>">Contact Us</a></li>
+                        <li class="active"><a href="<?php echo site_url('About')?>">About</a></li>
+                    </ul>
+                </div>
+                <!-- #Nav Ends -->
+
+            </div>
+        </div>
+
+    </div>
+    <!-- #Header Starts -->
+
+    <div class="container">
+
+        <!-- Header Starts -->
+        <div class="header">
+            <a href="<?php echo site_url()?>"><img src="<?php echo base_url('assets/images/logo1.png')?>" width="200px" alt="NgontraKuy"></a>
+
+
+            <?php if(isset($session) && $session == true){
+                echo
+                    "
+                <ul class=\"pull-right dropdown\">
+                    <li style=\"margin-top: 20px;\">
+                        <a href=\"#\" style=\"text-transform:Capitalize;font-family: 'Ubuntu', sans-serif;\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Hi, $nama! <span class=\"caret\"></span></a>
+                        <ul class=\"dropdown-menu\"  style=\"padding-top: 10px;padding-bottom: 10px;\">
+                            <li><a href=".site_url('Profile').">Profile</a></li>
+                            <li><a href=".site_url('Inbox').">Inbox</a></li>
+                            <li><a href=".site_url('Logout').">Log out</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                ";
+            }else{
+                echo "
+                <ul class=\"pull-right\">
+                    <li style=\"margin-top: 20px\"><a href=\"#\" style=\"font-family: 'Ubuntu', sans-serif;\" data-toggle=\"modal\" data-target=\"#loginpop\">Login</a></li>
+                </ul>";
+            } ?>
+        </div>
+        <!-- #Header Starts -->
+    </div>
+    <div class="inside-banner">
+        <div class="container">
+            <span class="pull-right"><a href="#">Home</a> / About</span>
+            <h2>About Us</h2>
+        </div>
+    </div>
+    <div class="container">
+        <div class="spacer">
+            <div class="row">
+                <div class="col-lg-8  col-lg-offset-2">
+                    <center><img src="<?php echo base_url('assets/images/logo1.png')?>" class="img-responsive thumbnail"  alt="realestate"></center>
+                    <h3>Business Background</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <h3>Company Profile</h3>
+                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                    <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+<?php
+#include footer file
+$this->view('template/footer');
+?>
