3b7f06bbaba0f02b2e3776a162e3a90f554637f5
zeinhanafi <1512501832@student.budiluhur.ac.id>
2018-04-18 16:16:46 +0700

    deleting function view

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -1,29 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Profile extends CI_Controller {
 
 	public function index()
 	{
 	    if($this->User->ceksession() == true){
+          $auth=$this->session->auth;
 	        $data['session'] = true;
           $data['iduser']=$this->session->userdata('iduser');
 	        $data['nama'] = $this->session->userdata('nama');
+          $data['auth']=$auth;
             $this->load->view('profiluser',$data);
         }else{
             $this->load->view('profiluser',$data);
         }
   }
-  public function view($iduser){
-    if($this->session->nama==null){
-      redirect('');
-    }
-    $nama=$this->session->nama;
-    $auth=$this->session->auth;
-    $result=$this->User->getUser($iduser);
-    $data['data']=$result;
-    $data['nama']=$nama;
-    $data['auth']=$auth;
-    $this->load->view('profiluser',$data);
-    }
+  
 }
