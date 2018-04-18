4b64444bcaddbea5ae198562d45570a98c06d4cc
chiachintyaa <chintyarifyaningrum@yahoo.com>
2018-04-18 20:41:58 +0700

    memperbaiki syntax untuk mengambil data dari session

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -6,15 +6,20 @@ class Profile extends CI_Controller {
 	public function index()
 	{
 	    if($this->User->ceksession() == true){
-          // die('masuk');
-          $auth=$this->session->auth;
+	        #validasi bahwa sessionnya masuk
 	        $data['session'] = true;
-          $iduser=$this->uri->segment(2);
-          $datauser=$this->User->getUser($iduser);
-          $data['datauser']=$datauser;
-	        $data['nama'] = $this->session->userdata('nama');
-          $data['auth']=$auth;
-            $this->load->view('profiluser',$data);
+
+	        #mindahin data session ke variabel baru untuk dipassing di view
+            $data['iduser']=$this->session->userdata('iduser');
+            $data['nama'] = $this->session->userdata('nama');
+            $data['auth']=$this->session->userdata('auth');
+
+            #nge get data user based on data session yang masuk
+            $data['datauser']=$this->User->getUser($data['iduser']);
+
+            if(isset($data['datauser'])){ #jika query berhasil dijalankan
+                $this->load->view('profiluser',$data); #jalankan view profil
+            }
         }else{
             redirect('/','refresh');
         }
