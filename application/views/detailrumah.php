a0c9ef3497083858786c11ba7fd99acfff11e1c2
zeinhanafi <1512501832@student.budiluhur.ac.id>
2018-04-18 14:18:08 +0700

    changing the header

--- application/views/detailrumah.php
+++ application/views/detailrumah.php
@@ -60,14 +60,32 @@ $this->view('template/header');
 
 <div class="container">
 
-    <!-- Header Starts -->
-    <div class="header">
-        <a href="<?php echo site_url()?>"><img src="<?php echo base_url('assets/images/logo1.png')?>" width="200px" alt="NgontraKuy"></a>
+  <!-- Header Starts -->
+  <div class="header">
+      <a href="<?php echo site_url()?>"><img src="<?php echo base_url('assets/images/logo1.png')?>" width="200px" alt="NgontraKuy"></a>
 
-        <ul class="pull-right">
-            <li style="margin-top: 20px"><a href="#" style="font-family: 'Ubuntu', sans-serif;" data-toggle="modal" data-target="#loginpop">Login</a></li>
-        </ul>
-    </div>
+
+          <?php if(isset($session) && $session == true){
+              echo
+                  "
+              <ul class=\"pull-right dropdown\">
+                  <li style=\"margin-top: 20px;\">
+                      <a href=\"#\" style=\"text-transform:Capitalize;font-family: 'Ubuntu', sans-serif;\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Hi, $nama! <span class=\"caret\"></span></a>
+                      <ul class=\"dropdown-menu\"  style=\"padding-top: 10px;padding-bottom: 10px;\">
+                          <li><a href=".site_url('Profile').">Profile</a></li>
+                          <li><a href=".site_url('Inbox').">Inbox</a></li>
+                          <li><a href=".site_url('Logout').">Log out</a></li>
+                      </ul>
+                  </li>
+              </ul>
+              ";
+          }else{
+              echo "
+              <ul class=\"pull-right\">
+                  <li style=\"margin-top: 20px\"><a href=\"#\" style=\"font-family: 'Ubuntu', sans-serif;\" data-toggle=\"modal\" data-target=\"#loginpop\">Login</a></li>
+              </ul>";
+          } ?>
+  </div>
     <!-- #Header Starts -->
 </div>
 
