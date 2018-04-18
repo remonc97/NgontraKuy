46abc34ead4c3b99ec853d8aedc2f11b893e3422
zeinhanafi <1512501832@student.budiluhur.ac.id>
2018-04-18 14:13:52 +0700

    changing the routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,6 +53,7 @@ $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
+$route['Home']='Home';
 $route['About'] = '';
 $route['Agents'] = '';
 $route['ContactUs'] = '';
