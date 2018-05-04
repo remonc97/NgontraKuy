9480babf4777eaa16f44806ee95942af11d5d866
refuza <raflyaswin31@gmail.com>
2018-05-04 07:13:48 +0700

    cuma echo

--- application/controllers/TaKon.php
+++ application/controllers/TaKon.php
@@ -110,7 +110,6 @@ class TaKon extends CI_Controller {
             }else{
 
                 echo "<script>alert('berhasil menambahkan kontrakan, namun gambar tidak dapat diupload. Coba beberapa saat lagi.')</script>";
-				redirect('Home');
             }
 	}
 
