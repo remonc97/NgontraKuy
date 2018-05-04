5c3385172f31d84c5b79196b9b838c111eb5e0c7
chiachintyaa <chintyarifyaningrum@yahoo.com>
2018-05-04 09:52:51 +0700

    fixed tagihan

--- application/controllers/Invoice.php
+++ application/controllers/Invoice.php
@@ -71,7 +71,7 @@ class Invoice extends CI_Controller
         $data = NULL;
         if ($result){
             $id = $this->session->userdata('idpengguna');
-            $data['all'] = $this->Model_Admin->getTagihan();
+            $data['all'] = $this->Tagihan->getTagihan();
             $data['user'] = $this->Model_Admin->get1User($id);
             $data['session'] = true;
             $data['namalengkap'] = $this->session->userdata('namalengkap');
