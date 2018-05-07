e0115e128af1353daf1c9e4d6bc4019371688c35
chiachintyaa <chintyarifyaningrum@yahoo.com>
2018-05-07 21:18:02 +0700

    added tracking google analytic

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -77,6 +77,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 </head>
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118776794-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-118776794-1');
+</script>
 
 <body>
 
